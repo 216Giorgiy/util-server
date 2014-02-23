5368475584047a392c8ab7ca2b92d94459ad45b5
Pamplemousse <xavier.maso@gmail.com>
2014-02-23 15:24:01 +0100

    change some settings for security

--- subsonic.sh
+++ subsonic.sh
@@ -5,13 +5,18 @@ msg(){
 }
 
 # Installing java (eurk)
-sudo apt-get install openjdk-7-jre
+apt-get install openjdk-7-jre
 msg 'Java jre installed'
 
 # Download and install subsonic
 wget http://downloads.sourceforge.net/project/subsonic/subsonic/4.9/subsonic-4.9.deb\?r\=\&amp\;ts\=1391970102\&amp\;use_mirror\=cznic
 mv subsonic-* subsonic.deb
 dpkg -i subsonic.deb
+mkdir /var/music
 msg 'Subsonic installed'
 
-msg 'Listening on port 40'
+adduser subsonic
+sed -i "s|^\(SUBSONIC_USER=root\).*$|\1 SUBSONIC_USER=subsonic|" /etc/default/subsonic
+sed -i '$ a DenyUsers subsonic' /etc/ssh/sshd_config
+
+msg 'Listening on port 4040'
